16da3cbe9ae582dcb318757b3d7ff69b4b7e917a
Mike May <mmay@px.ftw>
2015-08-12 09:02:11 -0400

    config interface for suricata from docker-compose.yml fixed

--- suricata/entrypoint.sh
+++ suricata/entrypoint.sh
@@ -1,19 +1,3 @@
-#!/usr/bin/env bash
-
-# wait for elasticsearch to start up - https://github.com/elasticsearch/kibana/issues/3077
-counter=0
-while [ ! "$(curl elasticsearch:9200 2> /dev/null)" -a $counter -lt 30  ]; do
-  sleep 1
-  ((counter++))
-  echo "waiting for Elasticsearch to be up ($counter/30)"
-done
-
-#
-echo "Staring log-courier"
-log-courier -config="/etc/log-courier/log-courier.conf" &
-
-# touch /var/log/suricata/eve.json
-# # chmod 644 /var/log/suricata/eve.json
-#
-# echo "Starting Suricata"
-# suricata -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid -k none -i eth0 &
+#!/bin/bash
+echo $INTERFACE
+suricata -c /etc/suricata/suricata.yaml -k none -i $INTERFACE --pidfile /var/run/suricata.pid
